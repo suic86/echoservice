8558d34ac99cce8b4b07be07558122041ec1ac8c
Gabriel Marko <marko.gabriel.cz@gmail.com>
2020-10-09 19:00:24 +0200

    Move debugging code to a separate file.

--- echoservice.py
+++ echoservice.py
@@ -12,23 +12,3 @@ def create_app():
         return jsonify({"timestamp": timestamp, "payload": payload})
 
     return app
-
-
-def _run():
-    from argparse import ArgumentParser
-
-    parser = ArgumentParser()
-    parser.add_argument(
-        "-p",
-        "--port",
-        type=int,
-        default=8080,
-        help="Port for the service to run. (default: 8080)",
-    )
-    args = parser.parse_args()
-    app = create_app()
-    app.run(host="0.0.0.0", port=args.port, debug=True)
-
-
-if __name__ == "__main__":
-    _run()
